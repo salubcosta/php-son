979ff4bf3f44ce1ae93a66577df3a23b6f142e80
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-14 23:48:54 -0300

    Primeira classe de tests apenas para testar o funcionamento do phpunit

---
+++ 004-PHP-solid/tests/HtmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CODE;
+
+class HtmlTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEsteEUmSimplesExemploDeTeste()
+    {
+        $this->assertTrue(true);
+    }
+}
