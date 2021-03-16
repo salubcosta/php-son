06b3cf46f85445a61fbdc81fe75d2e6b11be5ea3
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-15 21:58:43 -0300

    criado novos estes

--- 004-PHP-solid/tests/HtmlTest.php
+++ 004-PHP-solid/tests/HtmlTest.php
@@ -4,8 +4,29 @@ namespace CODE;
 
 class HtmlTest extends \PHPUnit\Framework\TestCase
 {
-    public function testEsteEUmSimplesExemploDeTeste()
+    public function testRenderizaUmaImagem()
     {
-        $this->assertTrue(true);
+        // $this->assertTrue(true);
+        $html = new Html;
+        $img = $html->img('images/photo.jpg');
+
+        $this->assertEquals("<img src=\"images/photo.jpg\">", $img);
+    }
+
+    public function testCriaUmLinkComImagem()
+    {
+        $html = new Html;
+        $img = $html->img('images/photo.jpg');
+        $link = $html->a('todev.com.br', $img);
+
+
+        $this->assertEquals("<a href=\"todev.com.br\"><img src=\"images/photo.jpg\"></a>", $link);
+    }
+
+    public function testListaNaoOrdenada()
+    {
+        $html = new Html;
+        $list = $html->ul('salumao');
+        $this->assertEquals("<ul><li>salumao</li></ul>", $list);
     }
 }
